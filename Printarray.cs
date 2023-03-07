12630230ac841af5454016f8bff7de161b65f7f5
saidatla201 <saidatla201@gmail.com>
2023-03-07 11:55:06 +0530

    Maximum Integer value

---
+++ Printarray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.cs
+{
+    //public  class Printarray
+    //{
+    //    private T[] inputArray;
+
+    //    public Printarray(T[] inputArray)
+    //    {
+    //        this.inputArray = inputArray;
+    //    }
+
+    //    public void toPrint()
+    //    {
+    //        foreach (var element in inputArray) ;
+    //        {
+    //            Console.WriteLine();
+    //        }
+    //    }
+    //}
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@
         {
             Console.WriteLine("Welcome to Generics");
 
+            Console.WriteLine("Enter first integer number");
+            int firstnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second integer number");
+            int secondnum  = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third integer number");
+            int thirdnum = Convert.ToInt32(Console.ReadLine());
+
+            int value = MaximumInteger.MaximumIntegerNumber(firstnum, secondnum, thirdnum);
+            Console.WriteLine("{0} is largest ", value);
+           
+
 
         }
     }
