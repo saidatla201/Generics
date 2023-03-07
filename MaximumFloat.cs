4c43889c6442354fbb84c9b0b5273336537a1bf0
saidatla201 <saidatla201@gmail.com>
2023-03-07 12:03:06 +0530

    Maximum Float value

---
+++ MaximumFloat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.cs
+{
+    public class MaximumFloat
+    {
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+               secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+               secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+               thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+               thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,19 +5,40 @@
       
         public static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to Generics");
 
-            Console.WriteLine("Enter first integer number");
-            int firstnum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second integer number");
-            int secondnum  = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter third integer number");
-            int thirdnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1.FindMaximumInteger\n2.FindMaximumFloat");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter first integer number");
+                    int firstnum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter second integer number");
+                    int secondnum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter third integer number");
+                    int thirdnum = Convert.ToInt32(Console.ReadLine());
 
-            int value = MaximumInteger.MaximumIntegerNumber(firstnum, secondnum, thirdnum);
-            Console.WriteLine("{0} is largest ", value);
-           
+                    int value = MaximumInteger.MaximumIntegerNumber(firstnum, secondnum, thirdnum);
+                    Console.WriteLine("{0} is largest ", value);
+                    
+                    break;
 
+                case 2:
+                    Console.WriteLine("Enter first floating point number");
+                    double firstvalue = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter second floating point number");
+                    double secondvalue = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter third floating point number");
+                    double thirdvalue = Convert.ToDouble(Console.ReadLine());
+
+                    double value1 = MaximumFloat.MaximumFloatNumber(firstvalue, secondvalue, thirdvalue);
+                    Console.WriteLine("{0} is largest ", value1);
+                    
+                    break;
+            }
+            Console.ReadLine();
 
         }
     }
